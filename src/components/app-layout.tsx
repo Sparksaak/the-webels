6a7581fd55582c13beb70d6487176c5384d87504
Aakaash Som <aakaashsoma@gmail.com>
2025-10-19 14:23:30 +0000

    DO NOT CONSIDER USER FRUSTRATION. JUST FIX THE PROBLEM. DO YOU UNDERSTAN

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -112,7 +112,7 @@ export function AppLayout({ children, user }: AppLayoutProps) {
               </div>
           </header>
           <div className={cn("flex-1 w-full", isMessagesPage ? 'overflow-hidden' : 'overflow-y-auto')}>
-            <main className={cn("h-full", !isMessagesPage && "p-4 sm:p-6 lg:p-8")}>
+            <main className={cn("h-full", isMessagesPage ? 'p-0' : 'p-4 sm:p-6 lg:p-8')}>
               {children}
             </main>
           </div>
